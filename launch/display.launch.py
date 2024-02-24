49aefcbada5867313ed13d0d21ff2c4049464af9
Andi Nur Nabila Syalwani <150206519+nabilasyalwani@users.noreply.github.com>
2024-02-25 02:21:41 +0800

    Update display.launch.py

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -3,25 +3,20 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.actions import ExecuteProcess
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command, PythonExpression
-from launch_ros.actions import LoadComposableNodes, SetParameter
-from launch_ros.descriptions import ComposableNode, ParameterFile
-from nav2_common.launch import RewrittenYaml
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
 
 from launch_ros.actions import Node
-
 
 def generate_launch_description():
     pkg_share = get_package_share_directory('finalproject')
     default_model_path = os.path.join(pkg_share, 'description/diff_drive_description.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/navigation.rviz')
     world_path=os.path.join(pkg_share, 'worlds/my_world.sdf')
-    slam_params_file = os.path.join(pkg_share,'config/localization','online_async_slam.yaml')
     
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -62,7 +57,7 @@ def generate_launch_description():
        executable='ekf_node',
        name='ekf_filter_node',
        output='screen',
-       parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+       parameters=[os.path.join(pkg_share, 'config/localization/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
 
     return LaunchDescription([
@@ -72,7 +67,7 @@ def generate_launch_description():
                                             description='Absolute path to rviz config file'),
         DeclareLaunchArgument(name='use_sim_time', default_value='True',
                                             description='Flag to enable use_sim_time'),
-        ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so',world_path], output='screen'),
+        ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
         joint_state_publisher_node,
         robot_state_publisher_node,
         spawn_entity,
